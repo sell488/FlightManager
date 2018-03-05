a789242108c17bb9e6a29b1f6a0be35c7bad2b47
sell488 <sell488sftoday@gmail.com>
2018-03-05 13:28:01 -0600

    Main Class touch up

--- src/sell488/flightmanager/Main.java
+++ src/sell488/flightmanager/Main.java
@@ -8,30 +8,9 @@ public class Main {
 
     //Main method
     public static void main(String[] args) {
-
-        int passengers;
-        int checkedBags;
-        int freeBags;
-        int space1 = 100;
-        int space2 = 300;
-        int space3 = 125;
-        int seats1 = 100;
-        final int FLIGHT_NUM_1 = 3210;
-        final int FLIGHT_NUM_2 = 2594;
-        final int FLIGHT_NUM_3 = 5867;
-        int NumCheckBagsIn1;
-        int NumCheckBagsIn2;
-        int NumCheckBagsIn3;
-        boolean done1 = false;
-        boolean flightDone1 = false;
-        int flightNumSet1 = -1;
-
-
-
-
         ArrayList <Flight> flightList = new ArrayList<>();
-
-
+        FlightManager flightManager = new FlightManager();
+        flightManager.addFlight(4321, 100,0,200);
 
     }
 
